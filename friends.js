3b00ee4e97b2f5dcae0f8a69a38d9f9e05951eaa
Athly23 <andrewly06@gmail.com>
2021-03-02 13:04:38 -0500

    added html, css styling and beginning js logic

---
+++ friends.js
@@ -0,0 +1,58 @@
+// const questions = [
+//   {
+//     "pic-url": "sample.jpeg",
+//     title: "rachel-birth",
+//     question: "What is the name of Ross and Rachel's baby?",
+//     answers: [
+//       { value: 0, answer: "Susan" },
+//       { value: 0, answer: "Ashley" },
+//       { value: 0, answer: "Emily" },
+//       { value: 5, answer: "Emma" },
+//     ],
+//   },
+//   {
+//     "pic-url": "group-picture.jpeg",
+//     title: "city",
+//     question: "Which city did the show take place in?",
+//     answers: [
+//       { value: 0, answer: "Boston" },
+//       { value: 0, answer: "Chicago" },
+//       { value: 0, answer: "San Diego" },
+//       { value: 5, answer: "New York City" },
+//     ],
+//   },
+// ];
+
+//Queing up the game with the start button
+
+const startButton = document.getElementById("start-btn");
+const questionContainerElement = document.getElementById("question-container"); //allows questions to be shown
+
+startButton.addEventListener("click", startGame);
+// function to start the game
+function startGame() {
+  console.log("started");
+  startButton.classList.add("hide"); //hiding start button
+  questionContainerElement.classList.remove("hide"); //shows question
+  setNextQuestion();
+}
+
+//function to set next question
+
+function setNextQuestion() {}
+
+// function for answer
+
+function selectAnswer() {}
+
+const questions = [
+  {
+    questions: "Which city did the show take place in?",
+    answers: [
+      { value: 0, answer: "Boston" },
+      { value: 0, answer: "Chicago" },
+      { value: 0, answer: "San Diego" },
+      { value: 5, answer: "New York City" },
+    ],
+  },
+];
