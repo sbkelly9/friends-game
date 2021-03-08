7ea399d82dd7eac87f6b042b0b50016f89c37f55
Stu Kelly <sbkelly9@gmail.com>
2021-03-08 18:12:01 -0500

    updated answer button with values

--- friends.js
+++ friends.js
@@ -26,7 +26,7 @@
 //Queing up the game with the start button
 
 const startButton = document.getElementById("start-btn");
-const questionContainerElement = document.getElementById("question-container"); //allows questions to be shown
+const questionContainerElement = document.getElementById("game-container"); //allows questions to be shown
 const gameQuestions = document.getElementById("questions");
 // const shuffleQuestions, currentQuestionsIndex;
 
@@ -59,4 +59,65 @@ const questions = [
       { value: 5, answer: "New York City" },
     ],
   },
+//   {
+//     question: "Which city did the show take place in?",
+//     answers: [
+//       { value: 0, answer: "Boston" },
+//       { value: 0, answer: "Chicago" },
+//       { value: 0, answer: "San Diego" },
+//       { value: 5, answer: "New York City" },
+//     ],
+//   },
+//   {
+//     question: "Which city did the show take place in?",
+//     answers: [
+//       { value: 0, answer: "Boston" },
+//       { value: 0, answer: "Chicago" },
+//       { value: 0, answer: "San Diego" },
+//       { value: 5, answer: "New York City" },
+//     ],
+//   },
+//   {
+//     question: "Which city did the show take place in?",
+//     answers: [
+//       { value: 0, answer: "Boston" },
+//       { value: 0, answer: "Chicago" },
+//       { value: 0, answer: "San Diego" },
+//       { value: 5, answer: "New York City" },
+//     ],
+//   },
 ];
+// iterate over the array to display questions and answers 
+let firstAnswerChoice = questions[0].answers[0].answer
+// console.log(firstAnswerChoice)
+
+let answerChoices = questions[0].answers
+// console.log(answerChoices)
+
+for (let i=0; i < answerChoices.length; i++){
+    // console.log(answerChoices[i].answer)
+}
+
+
+
+let body = document.querySelector('body')
+let questionContainer = document.querySelector('#questions-container')
+let newPTag = document.createElement('p')
+newPTag.innerHTML = questions[0].question
+questionContainer.appendChild(newPTag)
+
+
+
+
+let multipleChoiceButtons = document.querySelectorAll(".choice-buttons")
+console.log(multipleChoiceButtons)
+multipleChoiceButtons[0].innerHTML = firstAnswerChoice
+// console.log(multipleChoiceButtons[1].innerHTML)
+
+for (let i=0; i < multipleChoiceButtons.length; i++) {
+    let buttonText = multipleChoiceButtons[i]
+    let answerChoicesText = answerChoices[i].answer
+
+    multipleChoiceButtons[i].innerHTML = answerChoices[i].answer
+
+}
